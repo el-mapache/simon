3b6648c8efca24451d1735acf1ec9727daee0dee
Adam Biagianti <adam.biagianti@gmail.com>
2021-09-06 17:30:17 -0400

    remove state machines

--- src/stateMachine/statesMap.ts
+++
@@ -1,9 +0,0 @@
-enum MachineStates {
-  Idle = 'idle',
-  PlayPrompt = 'playPrompt',
-  NewGame = 'newGame',
-  ButtonPress = 'buttonPressed',
-  PatternChoice = 'patternChoice',
-}
-
-export default MachineStates;
