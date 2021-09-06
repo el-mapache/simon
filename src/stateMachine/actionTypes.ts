3ab982367a112b79fbe6dedd2ea56c76443abf1d
Adam Biagianti <adam.biagianti@gmail.com>
2021-09-06 17:30:17 -0400

    remove state machines

--- src/stateMachine/actionTypes.ts
+++
@@ -1,8 +0,0 @@
-enum MachineActionTypes {
-  NewGame = 'actions/newGame',
-  PromptInit = 'actions/init',
-  PromptPlay = 'actions/play',
-  UserInput = 'actions/userInput',
-}
-
-export default MachineActionTypes;
